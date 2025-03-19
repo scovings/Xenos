996103415062083dc1c00b36aad2d6f0aec72b19
sco. <77859647+scovings@users.noreply.github.com>
2025-03-19 17:44:54 +0000

    please fix it so there is no scrollbar

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -4,7 +4,14 @@ import './homepage.css'
 const Homepage = () => {
   return (
     <div className='homepage'>
-      <Link to="/dashboard">Dashboard</Link>
+      <img src="/orbital.png" alt="" className='orbital' />
+      <div className="left">
+        <h1>XENOS AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem.</h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right"></div>
     </div>
   )
 }
